b792f10f4e0b92be5fb27519d2fb08fc5c7a1037
Jose <jdvazquezdev@gmail.com>
2019-01-27 12:23:56 +0100

    First commit. Canvas and basic movement
    - Added basic canvas
    - Detected arrows keys pulsations
    - Added functions to draw the squares and let them moves.

---
+++ snake.js
@@ -0,0 +1,40 @@
+const myCanvas =document.getElementById('myCanvas');
+const context = myCanvas.getContext('2d');
+
+const SIZE = 20;
+
+const cuadrado = {
+	x: 0,
+	y: 0
+};
+
+setInterval(draw, 1000); //1000ms = 1s
+
+function draw() {
+	drawObject(cuadrado);
+	cuadrado.x += SIZE;
+}
+
+function drawObject(obj) {
+	context.fillRect(obj.x, obj.y, SIZE, SIZE);
+}
+
+// 'keypress' don't activate arrows keys
+document.addEventListener('keydown', moveSnake);
+
+function moveSnake(event) {
+	switch (event.key) {
+		case 'ArrowUp':
+			console.log('Move up');
+			break;
+		case 'ArrowDown':
+			console.log('Move down');
+			break;
+		case 'ArrowLeft':
+			console.log('Move left');
+			break;
+		case 'ArrowRight':
+			console.log('Move right');
+			break;
+	}
+}
